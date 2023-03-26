2d132f8617d259a10785ae623830e08d1c4c8f4f
zendy <nguyenduc.051194@gmail.com>
2023-03-26 13:46:55 +0700

    Connection to mysql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3309,
+      username: 'mysql',
+      password: 'mysql',
+      database: 'nestjs-typeorm',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
